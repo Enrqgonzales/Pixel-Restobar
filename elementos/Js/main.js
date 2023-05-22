1fd8a1bcd8130b07c8516dd6009f32733ad7210d
Codeflow <Enrqgonzales@github.com>
2023-05-21 20:08:45 -0500

    home section

--- elementos/Js/main.js
+++ elementos/Js/main.js
@@ -14,7 +14,12 @@ var homeSswiper = new Swiper(".home-swiper", {
     loop: 'true',
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
   });
 
 /*================ DEALS TAB ==================*/ 
