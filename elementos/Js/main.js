b70595be7df7ab28a6de739aead168aeef4ef01e
Codeflow <Enrqgonzales@github.com>
2023-07-05 18:49:11 -0500

    commit final

--- elementos/Js/main.js
+++ elementos/Js/main.js
@@ -1,10 +1,21 @@
 /*================ SHOW MENU ==================*/ 
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 /*=========== MENU SHOW ==========*/ 
-/*Validate if constant exists*/
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
 
 /*=========== MENU HIDDEN ==========*/ 
-/*Validate if constant exists*/
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
 
 /*================ REMOVE MENU MOBILE ==================*/ 
 
@@ -25,3 +36,10 @@ var homeSswiper = new Swiper('.home-swiper', {
 /*================ DEALS TAB ==================*/ 
 
 /*================ SHOW SCROLL UP ==================*/ 
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener ('scroll', scrollUp);
