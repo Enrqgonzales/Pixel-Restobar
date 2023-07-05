1d01ec61f9304b035bbe52772d55c626be0ef114
Codeflow <Enrqgonzales@github.com>
2023-07-04 19:38:37 -0500

    commit gallery

--- elementos/Js/main.js
+++ elementos/Js/main.js
@@ -9,17 +9,17 @@
 /*================ REMOVE MENU MOBILE ==================*/ 
 
 /*================ HOME SWIPER ==================*/ 
-var homeSswiper = new Swiper(".home-swiper", {
-    spaceBetween: 30,
-    loop: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+var homeSswiper = new Swiper('.home-swiper', {
+  spaceBetween: 30,
+  loop: 'true',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
   });
 
 /*================ DEALS TAB ==================*/ 
